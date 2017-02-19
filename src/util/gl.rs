ccf5559c36f0e087b4ee8fa4fc477a3cd89d3cbf
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2017-02-19 19:22:57 +0100

    Make shader loading more error proof

--- src/util/gl.rs
+++ src/util/gl.rs
@@ -1,6 +1,7 @@
 use errors::*;
 use glium::Program;
 use glium::backend::Facade;
+use std::path::Path;
 
 pub fn load_program<F, S>(facade: &F, src: S) -> Result<Program>
     where F: Facade,
@@ -11,20 +12,21 @@ pub fn load_program<F, S>(facade: &F, src: S) -> Result<Program>
 
     const SHADER_FOLDER: &'static str = "shader";
 
+    let shader_folder = Path::new(SHADER_FOLDER);
+
     // Load vertex shader
-    let vert_path = format!("{}/{}.vert", SHADER_FOLDER, src.vert_path());
-    debug!("Loading vertex shader '{}' ...", vert_path);
+    let vert_path = shader_folder.join(src.vert_path()).with_extension("vert");
+    debug!("Loading vertex shader '{}' ...", vert_path.display());
 
     let mut vert_buf = String::new();
     File::open(vert_path).and_then(|mut f| f.read_to_string(&mut vert_buf))?;
 
     // Load fragment shader
-    let frag_path = format!("{}/{}.frag", SHADER_FOLDER, src.frag_path());
-    debug!("Loading fragment shader '{}' ...", frag_path);
+    let frag_path = shader_folder.join(src.frag_path()).with_extension("frag");
+    debug!("Loading fragment shader '{}' ...", frag_path.display());
 
     let mut frag_buf = String::new();
     File::open(frag_path).and_then(|mut f| f.read_to_string(&mut frag_buf))?;
-
 
     // Link program
     debug!("Linking program ...");
@@ -42,14 +44,15 @@ pub fn load_program<F, S>(facade: &F, src: S) -> Result<Program>
 }
 
 pub trait ShaderSource {
-    fn vert_path(&self) -> &str;
-    fn frag_path(&self) -> &str;
+    fn vert_path(&self) -> &Path;
+    fn frag_path(&self) -> &Path;
 }
+
 impl<'a> ShaderSource for &'a str {
-    fn vert_path(&self) -> &str { self }
-    fn frag_path(&self) -> &str { self }
+    fn vert_path(&self) -> &Path { Path::new(self) }
+    fn frag_path(&self) -> &Path { Path::new(self) }
 }
-impl<'a> ShaderSource for (&'a str, &'a str) {
-    fn vert_path(&self) -> &str { self.0 }
-    fn frag_path(&self) -> &str { self.1 }
+impl<V: AsRef<Path>, F: AsRef<Path>> ShaderSource for (V, F) {
+    fn vert_path(&self) -> &Path { self.0.as_ref() }
+    fn frag_path(&self) -> &Path { self.1.as_ref() }
 }
