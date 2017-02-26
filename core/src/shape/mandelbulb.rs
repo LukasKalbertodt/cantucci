44b9dc0573840877952e2703cbbf0a9791f623ef
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2017-02-26 17:21:05 +0100

    Redesign `Shape` trait

--- core/src/shape/mandelbulb.rs
+++ core/src/shape/mandelbulb.rs
@@ -1,12 +1,15 @@
 use math::*;
-use super::{DistanceApprox, Shape};
+use super::Shape;
 
+// TODO: docs
 #[derive(Clone)]
 pub struct Mandelbulb {
     power: f32,
     max_iters: u64,
     bailout: f32,
 }
+
+const CENTER: Point3<f32> = Point3 { x: 0.0, y: 0.0, z: 0.0 };
 
 impl Mandelbulb {
     pub fn new(power: f32, max_iters: u64, bailout: f32) -> Self {
@@ -27,30 +30,12 @@ impl Shape for Mandelbulb {
         let mut z = p;
 
         for _ in 0..self.max_iters {
-            // Short alias for the current radius
-            let r = z.to_vec().magnitude();
-
             // If the radius is bigger than BAILOUT, this point will diverge
-            if r > self.bailout {
+            if (z - CENTER).magnitude() > self.bailout {
                 return false;
             }
 
-            // Convert to spherical coordinates
-            let theta = (z.z / r).acos();
-            let phi = f32::atan2(z.y, z.x);
-
-            // Scale and rotate the point
-            let zr = r.powf(self.power);
-            let theta = theta * self.power;
-            let phi = phi * self.power;
-
-            // Convert back to cartesian coordinates and add p
-            z = zr * Point3::new(
-                theta.sin() * phi.cos(),
-                phi.sin() * theta.sin(),
-                theta.cos(),
-            );
-            z += p.to_vec();
+            z = rotate(z, self.power) + (p - CENTER);
         }
 
         // The point didn't diverge within `max_iters`, so we assume it's in
@@ -58,43 +43,23 @@ impl Shape for Mandelbulb {
         true
     }
 
-    fn distance(&self, p: Point3<f32>) -> DistanceApprox {
+    fn min_distance_from(&self, p: Point3<f32>) -> f32 {
         let mut z = p;
         let mut dr = 1.0;
         let mut r = 0.0;
 
         for _ in 0..self.max_iters {
-            r = z.to_vec().magnitude();
+            r = (z - CENTER).magnitude();
             if r > self.bailout || (1.0 / r).is_infinite() {
                 break;
             }
 
-            // convert to polar coordinates
-            let theta = (z.z / r).acos();
-            let phi = f32::atan2(z.y, z.x);
             dr = r.powf(self.power - 1.0) * self.power * dr + 1.0;
-
-            // scale and rotate the point
-            let zr = r.powf(self.power);
-            let theta = theta * self.power;
-            let phi = phi * self.power;
-
-            // convert back to cartesian coordinates
-            z = zr * Point3::new(
-                theta.sin() * phi.cos(),
-                phi.sin() * theta.sin(),
-                theta.cos(),
-            );
-            z += p.to_vec();
+            z = rotate(z, self.power) + (p - CENTER);
         }
 
         let ln_r = if r.ln().is_infinite() { 0.0 } else { r.ln() * r };
-        let lower = 0.5 * ln_r / dr;
-
-        DistanceApprox {
-            min: lower,
-            max: 4.0 * lower,
-        }
+        0.5 * ln_r / dr
     }
 
     fn de_shader(&self) -> String {
@@ -105,4 +70,24 @@ impl Shape for Mandelbulb {
 
         s
     }
+}
+
+fn rotate(z: Point3<f32>, power: f32) -> Point3<f32> {
+    let old_radius = (z - CENTER).magnitude();
+
+    // Convert to spherical coordinates
+    let theta = (z.z / old_radius).acos();
+    let phi = f32::atan2(z.y, z.x);
+
+    // Scale and rotate the point
+    let new_radius = old_radius.powf(power);
+    let theta = theta * power;
+    let phi = phi * power;
+
+    // Convert back to cartesian coordinates and add p
+    new_radius * Point3::new(
+        theta.sin() * phi.cos(),
+        phi.sin() * theta.sin(),
+        theta.cos(),
+    )
 }
--- src/control/fly.rs
+++ src/control/fly.rs
@@ -98,10 +98,8 @@ impl CamControl for Fly {
             1.0
         };
 
-        trace!("distance: {} (from {:?})", shape.distance(self.cam.position).min, self.cam.position);
-
         let distance_multiplier = clamp(
-            2.0 * shape.distance(self.cam.position).min.abs(),
+            2.0 * shape.min_distance_from(self.cam.position).abs(),
             0.0,
             2.0
         );
