0ef142c927fe63927c2493a08049b0bb4ba38084
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2021-01-11 21:45:55 +0100

    Improve error reporting in build script

--- build.rs
+++ build.rs
@@ -1,10 +1,11 @@
-use std::{error::Error, fs, path::Path};
+use std::{fs, path::Path};
 
+use anyhow::{anyhow, bail, Context as _, Result};
 use shaderc::{Compiler, ShaderKind};
 
 
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> Result<()> {
     compile_shaders()?;
 
     Ok(())
@@ -16,7 +17,7 @@ const SHADERS: &[&str] = &[
     "sky.frag",
 ];
 
-fn compile_shaders() -> Result<(), Box<dyn Error>> {
+fn compile_shaders() -> Result<()> {
     let out_dir = Path::new(&std::env::var("OUT_DIR").unwrap()).join("shaders");
     if !out_dir.exists() {
         fs::create_dir(&out_dir)?;
@@ -30,6 +31,9 @@ fn compile_shaders() -> Result<(), Box<dyn Error>> {
             .join("shaders")
             .join(filename);
         let out_path = out_dir.join(format!("{}.spirv", filename));
+        if !full_path.exists() {
+            bail!("shader file '{}' does not exist", full_path.display());
+        }
 
         // If the spirv file is newer than the source file, we skip this shader.
         // Cargo makes sure the build script is only rerun if any of the shader
@@ -43,20 +47,22 @@ fn compile_shaders() -> Result<(), Box<dyn Error>> {
 
         // Figure out shader kind by file extension.
         let path = Path::new(filename);
-        let kind = match path.extension().ok_or("no extension in shader filename")?.to_str() {
+        let ext = path.extension().ok_or(anyhow!("no extension in shader filename"))?.to_str();
+        let kind = match ext {
             Some("vert") => ShaderKind::Vertex,
             Some("frag") => ShaderKind::Fragment,
-            _ => Err("invalid shader file extension")?,
+            _ => bail!("invalid shader file extension"),
         };
 
         // Actually compile shader and deal with errors.
-        let src = fs::read_to_string(&full_path)?;
+        let src = fs::read_to_string(&full_path)
+            .context(format!("failed to read '{}'", full_path.display()))?;
         let result = compiler.compile_into_spirv(&src, kind, filename, "main", None);
         let artifact = match result {
             Ok(v) => v,
             Err(shaderc::Error::CompilationError(_, msg)) => {
                 eprintln!("{}", msg);
-                Err("failed to compile shader")?
+                bail!("failed to compile shader '{}'", filename);
             }
             Err(e) => Err(e)?,
         };
@@ -66,7 +72,8 @@ fn compile_shaders() -> Result<(), Box<dyn Error>> {
         }
 
         // Write out result.
-        fs::write(out_path, artifact.as_binary_u8())?;
+        fs::write(&out_path, artifact.as_binary_u8())
+            .context(format!("failed to write '{}'", out_path.display()))?;
         println!("cargo:rerun-if-changed={}", full_path.display());
     }
 
